fdc94d7857582194f927013a4338ebf3cbd86230
Pablo Galeana Bailey <167713460+IngMatrix-PGB@users.noreply.github.com>
2024-12-13 13:27:27 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "remote" {
+    organization = "DevopsGalicia"
+
+    workspaces {
+      name = "DevopsGalicia"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "server-1" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  tags = {
+    Name   = "server"
+  }
+}
